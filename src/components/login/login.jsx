bf38dd6dbb73d1aa25644758a60bd684c6a2c2e2
beyzavdeprodesia <beyza.vdeprodesia@gmail.com>
2021-06-21 14:57:10 +0300

    register and login page example created

---
+++ src/components/login/login.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import loginImg from "../../assets/files/login.svg";
+
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Aday Giriş Paneli</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Kullanıcı Adı</label>
+              <input type="text" name="username" placeholder="Kullanıcı Adı" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Şifre</label>
+              <input type="password" name="password" placeholder="Şifre" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Giriş Yap
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
